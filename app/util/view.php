21983ac171eb3fc32053955874a157dfe9c8d47e
Jacob Lee <jake@seomoz.org>
2013-12-09 08:02:34 -0800

    Updated custom view renderer to actually do something, now runs a two step render, first the page contents, then the a container template to wrap the content.

--- app/util/view.php
+++ app/util/view.php
@@ -3,11 +3,26 @@
 namespace App\Util;
 
 class View extends \Slim\View {
-  public $parser;
-
-  public function __construct() {}
 
   public function render($template) {
-    
+    $data = $this->data->all();
+    $context = $data['context'];
+    $containerData = $data['container'];
+    $viewData = $data['view'];
+    $root = $this->getTemplatesDirectory();
+
+    $containerData['content'] = $this->sandbox("{$root}/{$context}/{$template}.php", $viewData);
+    return $this->sandbox("{$root}/{$context}/container.php", $containerData);
   }
+
+  public function sandbox($filename, $data) {
+    extract($data);
+    ob_start();
+    include $filename;
+    $content = ob_get_contents();
+    ob_end_clean();
+
+    return $content;
+  }
+
 }
